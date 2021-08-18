594f96fc71be0eafd32b0874c6164769f2a1a3b4
ajitkr09 <72242471+ajitkr09@users.noreply.github.com>
2021-08-18 22:05:39 +0530

    Update file_downloader.py
    New Links Can Be Fetched Now

--- file_downloader.py
+++ file_downloader.py
@@ -3,13 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 from pyfiglet import Figlet
 from termcolor import colored
-
 # PRINT
 f = Figlet(font='doom')
 print(colored(f.renderText('THE MOVIE DOWNLOADER') ,'red',))
 # GETTING THE WEBSITE
 searchable = input('please enter the movie or webseries to search: ')
-website_to_fire = "https://themoviesflix.io/"
+website_to_fire = "https://themoviesflix.in.net/"
 driver = webdriver.Chrome()
 driver.get(website_to_fire)
 search_box = driver.find_element_by_xpath('//*[@id="s"]').send_keys(searchable)
@@ -18,7 +17,7 @@ current_page_url = driver.current_url
 all_window_handles = driver.window_handles
 driver.switch_to.window(all_window_handles[1])
 driver.close()
-# GETTINF THE NAMES & LINKS OF WEBSERIES & WEB-SERIES
+# GETTINF THE NAMES & LINKS OF MOVIES & WEB-SERIES
 def names_and_links():
     ch = "category"
     website_to_scrape = current_page_url
@@ -29,19 +28,21 @@ def names_and_links():
     for row in names:  # Print all occurrences
         l = (row.get_text())
         print(l)
-        print("")
+        print(" ")
     for data in soup.find_all('article', class_='latestPost excerpt'):
         for a in data.find_all('a'):  # for getting the links
             r = (a.get('href'))
             if ch in r:
                 pass
             else:
+                print(" ")
                 print(r)
-                print()
 
 # GETTING EPISODES & SERVERS
 names_and_links()
+print(" ")
 url_to_click = input("please enter the link: ")
+print("")
 requests.get(url_to_click)
 # episodes
 website_to_scrape2 = url_to_click
@@ -50,12 +51,25 @@ soup2 = BeautifulSoup(source2,'lxml')
 for data in soup2.find_all('p',class_="has-text-align-center"):
     for link in data.find_all('a'):
         print(link.get('href'))
+        print(" ")
         print(link.get_text())
-for data2 in soup2.find_all('span',class_="maxbutton-3-container mb-container"):
+for data2 in soup2.find_all('span',class_="mb-center maxbutton-3-center"):
     for link2 in data2.find_all('a'):
-        print(link2.get('herf'))
-
-
-
-
-
+        print(link2.get('href'))
+        print(" ")
+        print(link2.get_text())
+for data3 in soup2.find_all('span',class_="maxbutton-6-container mb-container"):
+    for link3 in data3.find_all('a'):
+        print(link3.get('href'))
+        print(" ")
+        print(link3.get_text())
+for data4 in soup2.find_all('span',class_="maxbutton-13-container mb-container"):
+    for link4 in data4.find_all('a'):
+        print(link4.get('href'))
+        print(" ")
+        print(link4.get_text())
+for data5 in soup2.find_all('div',class_="wp-block-button aligncenter"):
+    for link5 in data5.find_all('a'):
+        print(link5.get('href'))
+        print(" ")
+        print(link5.get_text())
